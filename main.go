2d9590b1c1762dace465af3d4d8abf1167fe0b34
yusufmalikul <yusuflinuxer@gmail.com>
2021-01-23 14:05:56 +0700

    get full name and email

--- main.go
+++ main.go
@@ -1,43 +1,20 @@
 package main
 
 import (
+	"./handler"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"time"
 )
 
-type PageVariables struct {
-	Date    string
-	Time    string
-	Success bool
-}
-
 func main() {
-	http.HandleFunc("/", Home)
-	fmt.Println("listening on 8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	server := &http.Server{
+		Addr: fmt.Sprintf(":80"),
+		Handler: handler.New(),
 	}
-}
-
-func Home(w http.ResponseWriter, r *http.Request)  {
-	now := time.Now()
-	HomePageVars := PageVariables{
-		Date: now.Format("02-01-2006"),
-		Time: now.Format("15:04:05"),
-		Success: false,
-	}
-
-	t, err := template.ParseFiles("home.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = t.Execute(w, HomePageVars)
-	if err != nil {
+	log.Printf("Starting HTTP Server. Listening at %v", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
